8d06fa3a6c195420ab98e43377c531e87bbfb9db
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-10 19:28:19 +0200

    Add user profile section to main app

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,28 @@ class MyApp extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 150,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Benjamin Boswell',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Web Developer',
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
